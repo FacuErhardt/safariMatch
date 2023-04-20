421a2d13846f7a03362bd42cd3136ffc84e9a7d8
FacuErhardt <64286390+FacuErhardt@users.noreply.github.com>
2023-04-20 15:33:19 +0100

    agregando piezas

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,19 +8,46 @@ public class Board : MonoBehaviour
     public int height, width;
     public GameObject tileObject;
 
+    public float cameraSizeOffset;
+    public float cameraVerticalOffset;
+
+    public GameObject[] availablePieces;
+
     // Start is called before the first frame update
     void Start()
     {
         SetupBoard();
         PositionCamera();
+        SetupPieces();
 
+
+    }
+
+    private void SetupPieces()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var selectedPiece = availablePieces[UnityEngine.Random.Range(0, availablePieces.Length)];
+                var o = Instantiate(
+                    selectedPiece, new Vector3(x, y, -5), Quaternion.identity);
+                o.transform.parent = transform;
+                o.GetComponent<Pieces>()?.Setup(x, y, this);
+            }
+        }
     }
     private void PositionCamera()
     {
         float newPosX = (float)width / 2f;
         float newPosY = (float)height / 2f;
 
-        Camera.main.transform.position = new Vector3(newPosX -.5f, newPosY-.5f, -10f);
+        Camera.main.transform.position = new Vector3(newPosX -.5f, newPosY-.5f+ cameraVerticalOffset, -10f);
+
+        float horizontal = width + 1;
+        float vertical = (height / 2) + 1;
+
+        Camera.main.orthographicSize = horizontal > vertical ? horizontal + cameraSizeOffset : vertical + cameraSizeOffset;
     }
 
     private void SetupBoard()
@@ -32,10 +59,10 @@ public class Board : MonoBehaviour
                 var o = Instantiate(
                     tileObject, new Vector3(x, y, -5), Quaternion.identity);
                 o.transform.parent = transform;
+                o.GetComponent<Tile>()?.Setup(x, y, this);
             }
         }
-
-            
+      
     }
 
     // Update is called once per frame
